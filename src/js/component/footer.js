f14440c869f15b89dc917bfd1ef92e6fde1ae9a2
roncalloj <r-callo@hotmail.com>
2022-09-07 14:42:38 +0000

    cambio footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-mb-auto py-3 text-center">
 		<p className="p-0 m-0">
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="https://github.com/roncalloj">JCR</a>
 		</p>
 	</footer>
 );
